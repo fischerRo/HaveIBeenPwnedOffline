a85e7fda8354c3c20e62d0eb548d8500f08164e6
J-U-B <jub-git@mpi-halle.mpg.de>
2019-02-17 16:05:35 +0100

    added some error handling for files and csv parser
    * compatibility with python2.x for csv.DictReader
    * catching missing or unreadable password and/or csv files
    * handling missing/wrong columns in csv file
    * .gitignore for *.csv files

--- binary_search.py
+++ binary_search.py
@@ -2,15 +2,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import division, print_function, unicode_literals
 from hashlib import sha1
-from os import stat
+from os import stat, access, R_OK
 from argparse import ArgumentParser
 from getpass import getpass
+import sys
 import signal
 import csv
 
 def handler(signum, frame):
     print('\nCtrl-C pressed')
-    exit(0)
+    sys.exit(0)
 
 def binary_search(hex_hash, list_file, file_size):
     def get_full_line(file, pos):
@@ -92,24 +93,40 @@ if __name__ == "__main__":
     parser.add_argument('--csv-column-title', required=False, default="Title")
     parser.add_argument('--csv-column-password', required=False, default="Password")
     args = parser.parse_args()
+    if not access(args.password_file, R_OK):
+        raise SystemExit("***E*** Password hash file '%s' does not exist or is not readable!" % args.password_file)
     with open(args.password_file, 'r') as pwned_passwords_file:
         pwned_passwords_file_size = stat(args.password_file).st_size
         #print("File size: {} Bytes".format(pwned_passwords_file_size))
         if args.csv_file != "":
             print("Read passwords from CSV file: " + args.csv_file)
+            if not access(args.csv_file, R_OK):
+                raise SystemExit("***E*** Password hash file '%s' does not exist or is not readable!" % args.csv_file)
             with open(args.csv_file, 'rt') as csvfile:
-                csvreader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
+                delimiter=','
+                quotechar='"'
+                if 'decode' in dir(str):
+                    # workaround for python 2.x
+                    delimiter= delimiter.encode('ascii')
+                    quotechar= quotechar.encode('ascii')
+                csvreader = csv.DictReader(csvfile, delimiter=delimiter, quotechar=quotechar)
                 csvdata = [r for r in csvreader]
                 print("Entries found in CSV file: " + str(len(csvdata)))
                 for i in range(0, len(csvdata)):
-                    print("Entry #" + str(i) + ": " + csvdata[i][args.csv_column_group] + " : " + csvdata[i][args.csv_column_title] + " : " + csvdata[i][args.csv_column_user] + " : ", end='')
-                    if csvdata[i][args.csv_column_password].isdigit():
-                        print("(only " + str(len(csvdata[i][args.csv_column_password])) + " digits, potentially a PIN)")
-                    elif len(csvdata[i][args.csv_column_password]) > 0:
-                        check_pass(csvdata[i][args.csv_column_password], False)
+                    csvdata_user = csvdata[i][args.csv_column_user]  if (args.csv_column_user  in  csvdata[i].keys()) else "(no_user)"
+                    csvdata_group= csvdata[i][args.csv_column_group] if (args.csv_column_group in csvdata[i].keys()) else "(no_group)"
+                    csvdata_title= csvdata[i][args.csv_column_title] if (args.csv_column_title in csvdata[i].keys()) else "(no_title)"
+                    if not (args.csv_column_password in csvdata[i].keys()):
+                        print("***W*** Entry for user %s does not contain password column '%s'!" % (csvdata_user, args.csv_column_password))
                     else:
-                        print("(empty)")
-            raise SystemExit
+                        print("Entry #" + str(i) + ": " + csvdata_group + " : " + csvdata_title + " : " + csvdata_user + " : ", end='')
+                        if csvdata[i][args.csv_column_password].isdigit():
+                            print("(only " + str(len(csvdata[i][args.csv_column_password])) + " digits, potentially a PIN)")
+                        elif len(csvdata[i][args.csv_column_password]) > 0:
+                            check_pass(csvdata[i][args.csv_column_password], False)
+                        else:
+                            print("(empty)")
+            sys.exit(0)
         if (args.interactive==False) and (len(args.passwords) == 0):
             print ("No passwords given as argument.\n")
         for password in args.passwords:
@@ -121,5 +138,5 @@ if __name__ == "__main__":
             while True:
                 password=getpass('\nPassword: ')
                 if(password==''):
-                    exit(0)
+                    sys.exit(0)
                 check_pass(password, False)
